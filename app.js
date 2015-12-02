b073f1aa2a3ef870b080ac92671778b177e0b6ad
dschrag <dschrag@purdue.edu>
2015-12-02 18:06:26 -0500

    Added Database support to website.

--- app.js
+++ app.js
@@ -21,9 +21,49 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var cloudant = {
+	url: "https://64a24f2c-69f0-40f4-bd9c-04d6488fbaba-bluemix:cc6abd119705601eb695f5270baeaf5a01d00ed1c7179b44e65df40d9af95dfb@64a24f2c-69f0-40f4-bd9c-04d6488fbaba-bluemix.cloudant.com"
+};
+
+if (process.env.hasOwnProperty("VCAP_SERVICES")) {
+  // Running on Bluemix. Parse out the port and host that we've been assigned.
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var host = process.env.VCAP_APP_HOST;
+  var port = process.env.VCAP_APP_PORT;
+
+  // Also parse out Cloudant settings.
+  cloudant = env['cloudantNoSQLDB'][0].credentials;  
+}
+var nano = require('nano')(cloudant.url);
+var db = nano.db.use('sfe_scores');
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+app.get('/hiscores', function(request, response) {
+  db.view('top_scores', 'top_scores_index', function(err, body) {
+  if (!err) {
+    var scores = [];
+      body.rows.forEach(function(doc) {
+        scores.push(doc.value);		      
+      });
+      response.send(JSON.stringify(scores));
+    }
+  });
+});
+
+app.get('/save_score', function(request, response) {
+  var name = request.query.name;
+  var score = request.query.score;
+
+  var scoreRecord = { 'name': name, 'score' : parseInt(score), 'date': new Date() };
+  db.insert(scoreRecord, function(err, body, header) {
+    if (!err) {       
+      response.send('Successfully added one score to the DB');
+    }
+  });
 });
